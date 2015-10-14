4ae7f3f80993df59777aba9848cf3d72fb714570
Cristina Muntean <cristina.i.muntean@gmail.com>
2015-10-14 15:27:42 +0200

    added neutral users and cooccuring hashtags

--- stats/cooccuringHashtags.py
+++ stats/cooccuringHashtags.py
@@ -38,10 +38,11 @@ def tagUsers(tweetsAsDictionary):
 
     return [pro_refugee_users, anti_refugee_users, neutral_refugee_users, screen_name_dict]
 
-def coocuringTagsPerUsers(tweetsAsDictionary, pro_refugee_users, anti_refugee_users):
+def coocuringTagsPerUsers(tweetsAsDictionary, pro_refugee_users, anti_refugee_users, neutral_refugee_users):
     i=0
     usersWithPROHashtags = defaultdict(list)
     usersWithANTIHashtags = defaultdict(list)
+    usersWithNEUTRALHashtags = defaultdict(list)
 
     for tweet in tweetsAsDictionary:
         i+=1
@@ -54,7 +55,11 @@ def coocuringTagsPerUsers(tweetsAsDictionary, pro_refugee_users, anti_refugee_us
         elif tweet['user']['id_str'] in pro_refugee_users:
             hashtagList = [t for t in tweetText if ngrams.is_hashtag(t)]
             usersWithPROHashtags[tweet['user']['id_str']].extend(hashtagList)
-    return [usersWithPROHashtags, usersWithANTIHashtags]
+        elif tweet['user']['id_str'] in neutral_refugee_users:
+            hashtagList = [t for t in tweetText if ngrams.is_hashtag(t)]
+            usersWithNEUTRALHashtags[tweet['user']['id_str']].extend(hashtagList)
+
+    return [usersWithPROHashtags, usersWithANTIHashtags, usersWithNEUTRALHashtags]
 
 def writeOutput(dictUserHashtagList, outputFile):
     output = open(outputFile, 'w')
@@ -65,13 +70,14 @@ def writeOutput(dictUserHashtagList, outputFile):
 if __name__ == '__main__':
 
 
-    if len(sys.argv)!=5:
-        print "You need to pass the following 2 params: <tweetDirectory>  <userFile> <outputFileForUsersWithHashtagsPRO> <outputFileForUsersWithHashtagsANTI>"
+    if len(sys.argv)!=6:
+        print "You need to pass the following 2 params: <tweetDirectory>  <userFile> <outputFileForUsersWithHashtagsPRO> <outputFileForUsersWithHashtagsANTI> <outputFileForUsersWithHashtagsNEUTRAL>"
         sys.exit(-1)
     tweetDir = sys.argv[1]
     userFile = sys.argv[2]
     outputPRO = sys.argv[3]
     outputANTI = sys.argv[4]
+    outputNEUTRAL = sys.argv[5]
 
     logger = logging.getLogger("computerWordCount.py")
     logging.basicConfig(level=logging.DEBUG, format="%(asctime)s;%(levelname)s;%(message)s")
@@ -82,6 +88,7 @@ if __name__ == '__main__':
     userSets = tagUsers(tweetsAsDictionary)
     pos = userSets[0]
     neg = userSets[1]
+    neu = userSets[2]
     screen_names = userSets[3]
 
     writeOutput(screen_names, userFile)
@@ -89,10 +96,12 @@ if __name__ == '__main__':
     logger.info('Started computing coocurring hashtags per users')
 
     tweetsAsDict = Tweet(tweetDir).getTweetAsDictionary()
-    [usersWithPROHashtags, usersWithANTIHashtags] = coocuringTagsPerUsers(tweetsAsDict, pos, neg)
+    [usersWithPROHashtags, usersWithANTIHashtags, usersWithNEUTRALHashtags] = coocuringTagsPerUsers(tweetsAsDict, pos, neg, neu)
 
     writeOutput(usersWithPROHashtags, outputPRO)
     writeOutput(usersWithANTIHashtags, outputANTI)
+    writeOutput(usersWithNEUTRALHashtags, outputNEUTRAL)
+
 
     logger.info("Finished writing to file")
 
