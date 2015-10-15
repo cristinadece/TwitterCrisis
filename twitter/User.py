44f3d59ea480c09854df614a848bf779c41d4c24
Cristina Muntean <cristina.i.muntean@gmail.com>
2015-10-15 16:45:08 +0200

    test

--- twitter/User.py
+++ twitter/User.py
@@ -6,15 +6,13 @@ import os
 
 class User:
 
-    id = "";
-    type = "";
-    screen_name = ""
-    location = ""
-    tweet_locations = []
-    tweet_coordinates = []
-
     def __init__(self, id):
         self.id = id
+        self.type = "";
+        self.screen_name = ""
+        self.location = ""
+        self.tweet_locations = []
+        self.tweet_coordinates = []
 
     def __str__(self): # this is tricky
         return u"{0}\t{1}\t{2}\t{3}\t{4}\t{5}\n".format(self.id, self.type, self.screen_name, self.location, self.tweet_locations, self.tweet_coordinates).encode('utf-8')
