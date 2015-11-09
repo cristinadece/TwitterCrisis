719ba5623441a77c5a12a8343c9ecdcbf9c22c2c
Cristina Muntean <cristina.i.muntean@gmail.com>
2015-11-09 18:55:15 +0100

    minor fix parallel scripts

--- scripts/runParallelCoocHashtags.sh
+++ scripts/runParallelCoocHashtags.sh
@@ -1,5 +1,4 @@
-#!/usr/bin/env bash
-
+#!/bin/bash
 
 CORES=27
 #INPUT_DIR=/home/muntean/english-tweets-09
@@ -10,7 +9,7 @@ COMMAND="time python ../stats/cooccuringHashtags.py"
 for LINE in `ls $INPUT_DIR/*.gz`
 do
 	OUTPUT_NAME=`basename $LINE | cut -d'.' -f1`
-	sem -j $CORES $COMMAND $LINE $OUTPUT_DIR/hashtag-cooc-${OUTPUT_NAME}.output 
+	sem -j $CORES $COMMAND $LINE $OUTPUT_DIR/hashtag-cooc-${OUTPUT_NAME}.output
 done
 sem --wait
 exit 0
