3cc7c22787464385caae6d80fd1d987e7522ab92
Cristina Muntean <cristina.i.muntean@gmail.com>
2016-03-02 18:58:43 +0100

    minor fix ok

--- scripts/runParallelLocationsInTweets.sh
+++ scripts/runParallelLocationsInTweets.sh
@@ -2,13 +2,13 @@
 
 CORES=16
 INPUT_DIR=/home/muntean/relevant-tweets
-OUTPUT_DIR=/home/muntean/output-sem-filter
+OUTPUT_DIR=/home/muntean/locations-in-tweets-per-day
 COMMAND="time python ../stats/locationsInTweets.py"
 
-for LINE in `ls $INPUT_DIR/*.gz`
+for LINE in `ls $INPUT_DIR/*.output`
 do
 	OUTPUT_NAME=`basename $LINE | cut -d'.' -f1`
-	sem -j $CORES $COMMAND $LINE $OUTPUT_DIR/locations-in-tweets-${OUTPUT_NAME}.csv
+	sem -j $CORES $COMMAND $LINE $OUTPUT_DIR/locations-in-${OUTPUT_NAME}.csv
 done
 sem --wait
 exit 0
