27b3dfd7ef1de1080318f44f4018c3980ea3b128
Franco Maria Nardini <nardini@barbera2.isti.cnr.it>
2016-10-06 15:35:34 +0200

    new filter for Rome

---
+++ filters/filterRelevantTweets_Rome.py
@@ -0,0 +1,103 @@
+import codecs
+import json
+import logging
+import os
+import sys
+from collections import defaultdict
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+from twitter.Tweet import Tweet
+
+'''
+This is for geotagged raw JSON tweets
+'''
+
+romeBB = [tuple([12.341707, 41.769596]), tuple([12.626724, 42.01002])]
+
+def inBB(lon, lat, boundingbox=romeBB):
+    """
+    Checks if a coordinate [longitude, latitude] is within a Bounding Box
+    :param lon:
+    :param lat:
+    :param boundingbox:
+    :return:
+    """
+    lonMin = boundingbox[0][0]
+    lonMax = boundingbox[1][0]
+    latMin = boundingbox[0][1]
+    latMax = boundingbox[1][1]
+    return lonMin < lon < lonMax and latMin < lat < latMax
+
+
+def isBBinBB(tweet_bb, boundingbox=romeBB):
+    # [[33.9105011, -4.7672356], [33.9105011, 4.631608], [41.8998666, 4.631608], [41.8998666, -4.7672356]] - kenya
+    lonMin = tweet_bb[0][0]
+    lonMax = tweet_bb[2][0]
+    latMin = tweet_bb[0][1]
+    latMax = tweet_bb[2][1]
+    return inBB(lonMin, latMin, boundingbox) and inBB(lonMax, latMax, boundingbox)
+
+
+def isValidLocation(tweet):
+    tweet_place_bb = tweet["place"]["bounding_box"]["coordinates"][0]  # list of coordinates [longitude, latitude]
+    return isBBinBB(tweet_place_bb)
+
+
+# def getLocationData(tweet):
+#     """
+#     These can always have None values; e.g no coordinates, no city, no user location
+#     :param tweet:
+#     :return:
+#     """
+#     if tweet["coordinates"] is not None:
+#         tweet_coords = tweet['coordinates']['coordinates']  # returns a list [longitude, latitude]
+#     else:
+#         tweet_coords = None
+#
+#     if tweet["place"] is not None:
+#         if tweet["place"]["place_type"] == "city":
+#             tweet_place_city = tweet["place"]["name"]  # if place type == city
+#             tweet_place_country = tweet["place"]["country"]
+#             tweet_place_country_code = tweet["place"]["country_code"]
+#             tweet_place_bb = tweet["place"]["bounding_box"]["coordinates"]
+#         else:
+#             tweet_place_city = None
+#             tweet_place_country = tweet["place"]["country"]
+#             tweet_place_country_code = tweet["place"]["country_code"]
+#             tweet_place_bb = tweet["place"]["bounding_box"]["coordinates"]
+#     else:
+#         tweet_place_city = None
+#         tweet_place_country = None
+#         tweet_place_country_code = None
+#
+#     #user_location = tweet['user']['location']
+#
+#     return tweet_coords, tweet_place_city, tweet_place_country, tweet_place_country_code
+
+def dumpDictValuesToFile(tweet, file):
+    line = json.dumps(tweet) + "\n"
+    file.write(line)
+
+if __name__ == '__main__':
+    logger = logging.getLogger("filterRelevantTweetsFM.py")
+    logging.basicConfig(level=logging.DEBUG, format="%(asctime)s;%(levelname)s;%(message)s")
+
+    if len(sys.argv) != 3:
+        print "You need to pass the following 2 params: <jsonTweetsFile> <filteredTweetsFile> "
+        sys.exit(-1)
+    inputFile = sys.argv[1]
+    outputRelevant = codecs.open(sys.argv[2], "w", "utf-8")
+
+
+    # print htDict
+    tweetsAsDict = Tweet.getTweetAsDictionary(inputFile)
+
+    i = 0
+    for tweet in tweetsAsDict:
+        if tweet["coordinates"] is not None:
+            tweet_coords = tweet['coordinates']['coordinates']  # returns a list [longitude, latitude]
+            if (inBB(tweet_coords[0], tweet_coords[1])):
+                dumpDictValuesToFile(tweet, outputRelevant)
+        else:
+            tweet_coords = None
+            
+    outputRelevant.close()
