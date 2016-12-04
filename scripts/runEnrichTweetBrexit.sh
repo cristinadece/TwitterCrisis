d9b768b40dbc8078079e26c63e272431f1a27367
muntean <cristina.i.muntean@gmail.com>
2016-12-04 20:33:13 +0100

    del brexit kw

--- scripts/runEnrichTweetBrexit.sh
+++ scripts/runEnrichTweetBrexit.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
-CORES=16
-INPUT_DIR=/data/muntean/brexit-filtered
-OUTPUT_DIR=/data/muntean/brexit-enriched
+CORES=17
+INPUT_DIR=/home/muntean/brexit-filtered
+OUTPUT_DIR=/home/muntean/brexit-enriched
 COMMAND="time python enrich_relevant_tweet.py"
 
 for LINE in `ls $INPUT_DIR/*.output`
 do
 	OUTPUT_NAME=`basename $LINE | cut -d'.' -f1`
-	sem -j $CORES $COMMAND $LINE $OUTPUT_DIR/enriched-brexit-in${OUTPUT_NAME}.output
+	sem -j $CORES $COMMAND $LINE $OUTPUT_DIR/enriched-brexit-in-${OUTPUT_NAME}.output
 done
 sem --wait
 exit 0
